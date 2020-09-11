3d55c1b88a76748bfe11da5f8eea73e81856ab67
KarstenBlok <55189590+KarstenBlok@users.noreply.github.com>
2020-09-11 12:12:49 +0200

    Add files via upload

---
+++ Eersteoefening.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Eersteoefening {
+    public static void main (String[] args){
+        JOptionPane.showMessageDialog(null, "Hello World!");
+    }
+}
+
+
---
+++ Oefenen2.java
@@ -0,0 +1,11 @@
+public class Oefenen2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        int i; // Declaratie
+        i = 7;
+        System.out.println(i);
+        long y;
+        y = 782785;
+    }
+}
